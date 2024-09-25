2066959f37b2ae96c7fce75d8fece3847f97f758
jmacker101 <jm470t@gmail.com>
2024-09-25 03:50:22 -0400

    adding animation to certain aspects to Hero.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from "./components/Hero"
 import Technologies from "./components/Technologies"
 import Projects from "./components/Projects"
 import Experience from "./components/Experience"
+import Contact from "./components/Contact"
 
 
 const App = () => {
@@ -21,6 +22,7 @@ const App = () => {
         <Technologies />
         <Projects />
         <Experience />
+        <Contact />
       </div>
     </div>
   )
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,29 +1,66 @@
 import ProfilePic from "../assets/ProfilePic.jpg"
 import { HERO_CONTENT } from "../constants";
+import { motion, stagger } from "framer-motion"
+
+const containerVariants = {
+    hidden: { opacity: 0, x: -100 },
+    visible: {
+        opacity: 1,
+        x: 0,
+        transition: {
+            duration: 0.5,
+            staggerchildren: 0.5,
+        }
+    }
+}
+const childVariants = {
+    hidden: { opacity: 0, x: -100 },
+    visible: { opacity: 1, x: 0, trasition: { duration: 0.5 }}
+}
 const Hero = () => {
   return (
     <div className="pb-4 lg:mb-36">
         <div className="flex flex-wrap lg:flex-row-reverse">
             <div className="w-full lg:w-1/2">
                 <div className="flex justify-center lg:p-8">
-                    <img src={ProfilePic} alt="Jeovan Mills" className="border border-stone-900 rounded-3xl"/>
+                    <motion.img 
+                    src={ProfilePic} 
+                    alt="Jeovan Mills" 
+                    className="border border-stone-900 rounded-3xl"
+                    width={650}
+                    height={650}
+                    initial={{ x: 100, opacity: 0}}
+                    animate= {{ x: 0, opacity: 1 }}
+                    transition={{ duration: 1, delay: 1.5 }} />
                 </div>
             </div>
             <div className="w-full lg:w-1/2">
-                <div className="flex flex-col items-center lg:items-start mt-10">
-                    <h2 className="pb-2 text-4xl tracking-tighter lg:text-8xl">Jeovan Mills</h2>
-                    <span className="bg-gradient-to-r from-stone-300 to-stone-600 bg-clip-text text-3x1 tracking-tight text-transparent">Full Stack Developer</span>
-                    <p className="my-2 max-w-lg py-6 text-xl leading-relaxed tracking-tighter">
+                <motion.div
+                initial="hidden"
+                animate="visible"
+                variants={containerVariants}
+                className="flex flex-col items-center lg:items-start mt-10">
+                    <motion.h2
+                    variants={childVariants}
+                     className="pb-2 text-4xl tracking-tighter lg:text-8xl">Jeovan Mills</motion.h2>
+                    <motion.span 
+                    variants={childVariants}
+                    className="bg-gradient-to-r from-stone-300 to-stone-600 bg-clip-text text-4xl tracking-tight text-transparent">Full Stack Developer</motion.span>
+                    <motion.p 
+                    variants={childVariants}
+                    className="my-2 max-w-lg py-6 text-xl leading-relaxed tracking-tighter">
                         {HERO_CONTENT}
-                    </p>
-                    <a href="/resume.pdf"
+                    </motion.p>
+                    <motion.a 
+                    variants={childVariants}
+                    href="/resume.pdf"
                     target="_blank"
                     rel="noopener noreferrer"
                     download 
                     className="bg-white rounded-full p-4 text-sm text-stone-800 mb-10">
                         Download Resume
-                    </a>
-                </div>
+                    </motion.a>
+                </motion.div>
             </div>
         </div>
     </div>
